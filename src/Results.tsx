3bb7fb1ddc4323c06a027ed7b0274c29d40ba956
Richard Misiak <richard.h.misiak@outlook.com>
2024-11-10 17:46:22 +0000

    adjust handling of unranked skills

--- src/Results.tsx
+++ src/Results.tsx
@@ -3,15 +3,31 @@ import { useState } from "preact/hooks";
 import { ColumnConfig, StatsResponse, FormattedSkillData } from "./types";
 import { baseUrl } from "./api";
 
+const numberFormatter = (x: number) =>
+  x === -1 ? <em style={{ opacity: "50%" }}>unranked</em> : x.toLocaleString();
+
 const columns: ColumnConfig[] = [
   { key: "name", title: "Name" },
-  { key: "level", title: "Level" },
-  { key: "xp", title: "XP", formatter: (x: number) => x.toLocaleString() },
-  { key: "rank", title: "Rank", formatter: (x: number) => x.toLocaleString() },
+  {
+    key: "level",
+    title: "Level",
+    formatter: numberFormatter,
+  },
+  {
+    key: "xp",
+    title: "XP",
+    formatter: numberFormatter,
+  },
+  {
+    key: "rank",
+    title: "Rank",
+    formatter: numberFormatter,
+  },
   {
     key: "percent",
     title: "Percent to 99",
-    formatter: (x: number) => (Math.round(x * 10) / 10).toFixed(1),
+    formatter: (x: number) =>
+      x === -1 ? numberFormatter(x) : (Math.round(x * 10) / 10).toFixed(1),
   },
 ];
 
@@ -40,7 +56,8 @@ export const Results: FunctionalComponent<{ data: StatsResponse }> = ({
   const withPercent: FormattedSkillData[] = skills
     .map((x) => ({
       ...x,
-      percent: (100 * Math.min(x.xp, maxXp)) / maxXp,
+      level: x.rank === -1 ? -1 : x.level,
+      percent: x.rank === -1 ? -1 : (100 * Math.min(x.xp, maxXp)) / maxXp,
     }))
     .sort((a: Record<string, any>, b: Record<string, any>) => {
       if (!(sortDirection && sortHeader)) {
@@ -61,8 +78,10 @@ export const Results: FunctionalComponent<{ data: StatsResponse }> = ({
     });
 
   const overall =
-    withPercent.reduce((acc, next) => (acc += next.percent), 0) /
-    withPercent.length;
+    withPercent.reduce(
+      (acc, next) => (acc += next.percent == -1 ? 0 : next.percent),
+      0
+    ) / withPercent.length;
 
   const overallRoundex = Math.round(overall * 10) / 10;
 
@@ -112,13 +131,7 @@ export const Results: FunctionalComponent<{ data: StatsResponse }> = ({
                     {i === 0 && (
                       <img src={`${baseUrl}?skill=${s[c.key]}`}></img>
                     )}
-                    {c.key === "rank" && s[c.key] === -1 ? (
-                      <em style={{ opacity: "50%" }}>unranked</em>
-                    ) : c.formatter ? (
-                      c.formatter(s[c.key])
-                    ) : (
-                      s[c.key]
-                    )}
+                    {c.formatter ? c.formatter(s[c.key]) : s[c.key]}
                   </div>
                 </td>
               ))}
